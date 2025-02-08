b440207dfd7eaabe40b6464b015b3cb766bef1ca
Peter GAO <37321872+petergaobro@users.noreply.github.com>
2025-02-08 16:04:59 +1100

    myapp init commit, change view

--- one/app/(tabs)/index.tsx
+++ one/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Hello world by Peter and Wiki</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
